7f74f4cbe2da6be1547b5810d29b7062e2d3e4cb
Manoharthotla <manoharthotla777@gmail.com>
2025-12-29 21:39:59 +0530

    third commit

--- server.js
+++ server.js
@@ -178,7 +178,5 @@ function escapeHTML(text) {
 }
 
 /* ---------------- START SERVER ---------------- */
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+module.exports = app;
+
